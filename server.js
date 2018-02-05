1be94d9e34fefa7f662de83a4b3bbd50c846d831
unknown <vargasce@Programador.lafabril.com.do>
2018-02-05 09:31:32 -0400

    Add project page

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
 //register middleware
 app.use((req, res, next) => {
   var now = new Date().toString();
@@ -23,11 +25,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
-
 app.use(express.static(__dirname + '/public'));
+
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -53,8 +53,14 @@ app.get('/about', (req, res) => {
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request!'
-  })
-})
+  });
+});
+
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is up in port ${port}`);
